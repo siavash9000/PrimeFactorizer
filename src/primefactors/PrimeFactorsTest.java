b4f5c5627983432eb18f2b13cc14ce580b89d5c6
siavash9000 <siavash@siavash-ThinkPad-X230.(none)>
2014-05-04 19:13:48 +0200

    minor refactoring of test

--- src/primefactors/PrimeFactorsTest.java
+++ src/primefactors/PrimeFactorsTest.java
@@ -13,22 +13,6 @@ public class PrimeFactorsTest {
 	public void setup() {
 		factorizer = new PrimeFactorizer();
 	}
-
-	@Test
-	public void smallestPrimeNumberTest() {
-		Integer smallestPrimeNUmber = 2;
-		ArrayList<Integer> expected = new ArrayList<Integer>();
-		expected.add(smallestPrimeNUmber);
-		Assert.assertEquals(expected,factorizer.factorize(smallestPrimeNUmber));
-	}
-	
-	@Test
-	public void oneTest() {
-		Integer one = 1;
-		ArrayList<Integer> expected = new ArrayList<Integer>();
-		expected.add(one);
-		Assert.assertEquals(expected,factorizer.factorize(one));
-	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void invalidDataZeroTest() {
@@ -42,4 +26,21 @@ public class PrimeFactorsTest {
 		Integer randomNegativeNumber = -randomNumberGenerator.nextInt(100000);
 		factorizer.factorize(randomNegativeNumber);
 	}
+
+	@Test
+	public void oneTest() {
+		Integer one = 1;
+		ArrayList<Integer> expected = new ArrayList<Integer>();
+		expected.add(one);
+		Assert.assertEquals(expected,factorizer.factorize(one));
+	}
+	
+	@Test
+	public void twoTest() {
+		Integer two = 2;
+		ArrayList<Integer> expected = new ArrayList<Integer>();
+		expected.add(two);
+		Assert.assertEquals(expected,factorizer.factorize(two));
+	}
+
 }
