0951361d08440d29186ee59b07a1d33f742424f1
siavash <siavash@siavashs-iMac.local>
2014-05-10 23:53:36 +0200

    first parallelization via threadpool,3 times faster on 4 cores

--- src/main/java/primefactorizer/parallel/TrialDivisionExecutorService.java
+++ src/main/java/primefactorizer/parallel/TrialDivisionExecutorService.java
@@ -3,11 +3,48 @@ package primefactorizer.parallel;
 import primefactorizer.IntegerFactorizerInterface;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class TrialDivisionExecutorService implements IntegerFactorizerInterface {
 
     private int numberOfCores = Runtime.getRuntime().availableProcessors();
+
+    private class SubFactorizer implements Runnable {
+        long toBeFactorized,start,end;
+        boolean computationFinished;
+        List<List<Long>> resultsContainer;
+        public SubFactorizer(long toBeFactorized,long start,long end, List<List<Long>> resultsContainer){
+            this.toBeFactorized=toBeFactorized;this.start=start;this.end=end;this.resultsContainer=resultsContainer;
+            this.computationFinished=false;
+        }
+
+        @Override
+        public void run() {
+            resultsContainer.add(findFactorsInRange(toBeFactorized,start,end));
+            computationFinished=true;
+        }
+
+        private List<Long> findFactorsInRange(long toBeFactorized, long start, long end) {
+            ArrayList<Long> primeFactors = new ArrayList<Long>();
+            long factorCandidate=start;
+            while(factorCandidate <=  end){
+                while(toBeFactorized % factorCandidate == 0) {
+                    primeFactors.add(factorCandidate);
+                    toBeFactorized /= factorCandidate;
+                }
+                factorCandidate++;
+            }
+            //toBeFactorized must be prime now or 1
+            if(toBeFactorized>1){
+                primeFactors.add(toBeFactorized);
+            }
+            return primeFactors;
+        }
+    }
 
     @Override
     public List<Long> factorize(long toBeFactorized) {
@@ -15,56 +52,69 @@ public class TrialDivisionExecutorService implements IntegerFactorizerInterface 
             throw new IllegalArgumentException("Argument must be greater 0.");
         }
         if (toBeFactorized<1000) {
-            return findFactorsInRange(toBeFactorized,2,toBeFactorized);
+            ArrayList<List<Long>> partialResults = new ArrayList<List<Long>>();
+            SubFactorizer solver = new SubFactorizer(toBeFactorized,2,toBeFactorized,partialResults);
+            solver.run();
+            return partialResults.get(0);
         }
-        ArrayList<List<Long>> partialResults = solveSubProblems(toBeFactorized);
-        return merge(partialResults);
+        List<List<Long>> partialResults = solveSubProblems(toBeFactorized);
+        return merge(partialResults,toBeFactorized);
     }
 
-    private ArrayList<List<Long>> solveSubProblems(long toBeFactorized) {
+    private List<List<Long>> solveSubProblems(long toBeFactorized) {
         long upperbound = (long) Math.ceil(Math.sqrt(toBeFactorized));
         long intervalLength = upperbound / numberOfCores;
-        ArrayList<List<Long>> partialResults = new ArrayList<List<Long>>();
-        partialResults.add(findFactorsInRange(toBeFactorized,2,intervalLength));
+        List<List<Long>> partialResults = new ArrayList<List<Long>>();
+        List<SubFactorizer> subProblems = new ArrayList<SubFactorizer>();
+        subProblems.add(new SubFactorizer(toBeFactorized,2,intervalLength,partialResults));
         for(int i=1;i<numberOfCores-1;i++){
-            partialResults.add(findFactorsInRange(toBeFactorized,intervalLength*i+1,intervalLength*(i+1)));
+            subProblems.add(new SubFactorizer(toBeFactorized,intervalLength*i+1,intervalLength*(i+1),partialResults));
         }
-        partialResults.add(findFactorsInRange(toBeFactorized,intervalLength*numberOfCores,upperbound));
+        subProblems.add(new SubFactorizer(toBeFactorized,intervalLength*numberOfCores,upperbound,partialResults));
+        ExecutorService executor = Executors.newCachedThreadPool();
+        for (SubFactorizer subProblem: subProblems){
+            executor.execute(subProblem);
+        }
+        while(!allSubproblemsSolved(subProblems));
+        executor.shutdown();
         return partialResults;
     }
 
-    private List<Long> findFactorsInRange(long toBeFactorized, long start, long end) {
-        ArrayList<Long> primeFactors = new ArrayList<Long>();
-        long factorCandidate=start;
-        while(factorCandidate <=  end){
-            while(toBeFactorized % factorCandidate == 0) {
-                primeFactors.add(factorCandidate);
-                toBeFactorized /= factorCandidate;
-            }
-            factorCandidate++;
+    private boolean allSubproblemsSolved(List<SubFactorizer> subProblems) {
+        boolean finished = true;
+        for(SubFactorizer subProblem: subProblems){
+            finished=finished&&subProblem.computationFinished;
         }
-        //toBeFactorized must be prime now or 1
-        if(toBeFactorized>1){
-            primeFactors.add(toBeFactorized);
-        }
-        return primeFactors;
+        return finished;
     }
-
-    private List<Long> merge(List<List<Long>> partialResults) {
+    private List<Long> merge(List<List<Long>> partialResults,long toBeFactorized) {
+        ArrayList<Long> resultCandidates = new ArrayList<Long>();
+        for (List<Long> partial: partialResults){
+            for(Long value:partial) {
+                resultCandidates.add(value);
+            }
+        }
+        Collections.sort(resultCandidates);
         ArrayList<Long> result = new ArrayList<Long>();
-        for(int i=0;i<partialResults.size();i++){
-            for (Long candidate: partialResults.get(i)) {
-                boolean candidateRedundant = false;
-                for (int j = 0; j < i; j++) {
-                    for (Long alreadyVerifiedElement: partialResults.get(j)) {
-                        if (candidate % alreadyVerifiedElement == 0) {
+        for(int i=0;i<resultCandidates.size();i++){
+            boolean candidateRedundant = false;
+            long productOfValue = resultCandidates.get(i);
+            for (int j = 0; j < i; j++) {
+                if (resultCandidates.get(i) % resultCandidates.get(j) == 0) {
+                    if(resultCandidates.get(i)==resultCandidates.get(j)) {
+                        productOfValue=productOfValue*resultCandidates.get(i);
+                        if (toBeFactorized%productOfValue!=0){
                             candidateRedundant = true;
                         }
                     }
+                    else {
+                        candidateRedundant = true;
+                    }
                 }
-                if (!candidateRedundant) {
-                    result.add(candidate);
-                }
+
+            }
+            if (!candidateRedundant) {
+                result.add(resultCandidates.get(i));
             }
         }
         return result;
